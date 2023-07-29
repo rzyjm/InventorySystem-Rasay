dafaab4d4b220ff1ddd1586c7c8a5c3e677dac18
rzyjm <rasayjamesm@gmail.com>
2023-07-29 13:54:36 +0800

    Add class

--- InventorySystem-Rasay/Program.cs
+++ InventorySystem-Rasay/Program.cs
@@ -1,70 +1,26 @@
-﻿using System;
+﻿// Rasay, James MIguel A.
+// BSCPE BN 1-1
+
+using InventorySystem_Rasay;
+using System;
 using System.Collections.Generic;
 
-class Program
+class Inventory
 {
-    static List<string> snacks = new List<string>();
-    static List<string> bathEssentials = new List<string>();
-    static List<string> cannedGoods = new List<string>();
-    static List<string> beverages = new List<string>();
+    private List<string> snacks;
+    private List<string> bathEssentials;
+    private List<string> cannedGoods;
+    private List<string> beverages;
 
-    static string pinCode = "0221";
-
-    public static void Main(string[] args)
+    public Inventory()
     {
-        Console.Write("Enter PIN Code: ");
-        string userInput = Console.ReadLine();
-
-        while (userInput != "0")
-        {
-            if (userInput == pinCode)
-            {
-                Console.WriteLine("~ Welcome to Inventory ~");
-                Console.WriteLine("_____________________");
-                Console.WriteLine("Enter 1 to View Inventory");
-                Console.WriteLine("Enter 2 to Deposit Items");
-                Console.WriteLine("Enter 3 to Exit Inventory");
-                Console.WriteLine("-------------------------");
-                Console.Write("Enter your choice: ");
-                userInput = Console.ReadLine();
-
-                switch (userInput)
-                {
-                    case "1":
-                        ViewInventory();
-                        break;
-                    case "2":
-                        DepositItems();
-                        break;
-                    case "3":
-                        Console.WriteLine("Exiting Inventory...");
-                        return;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid PIN Code. Please try again.");
-            }
-
-            Console.WriteLine("_____________________");
-            Console.Write("Do you want to continue (y/n)? ");
-            string continueChoice = Console.ReadLine();
-
-            if (continueChoice.ToLower() == "n")
-            {
-                Console.WriteLine("Exiting Inventory...");
-                return;
-            }
-
-            Console.Write("Enter PIN Code: ");
-            userInput = Console.ReadLine();
-        }
+        snacks = new List<string>();
+        bathEssentials = new List<string>();
+        cannedGoods = new List<string>();
+        beverages = new List<string>();
     }
 
-    public static void ViewInventory()
+    public void ViewInventory()
     {
         Console.WriteLine("Inventory:");
         Console.WriteLine("Snacks:");
@@ -120,15 +76,8 @@ class Program
         }
     }
 
-    public static void DepositItems()
+    public void DepositItems(string category, string itemName)
     {
-        Console.WriteLine("~~~Deposit Items~~~");
-        Console.WriteLine("Enter the category [Snacks, Bath Essentials, Canned Goods, or Beverages]: ");
-        string category = Console.ReadLine();
-
-        Console.WriteLine("Enter the item name: ");
-        string itemName = Console.ReadLine();
-
         switch (category)
         {
             case "Snacks":
@@ -151,5 +100,76 @@ class Program
                 Console.WriteLine("Invalid category. Please choose a valid category.");
                 break;
         }
+    }
+}
+class Program
+{
+    static string pinCode = "0221";
+    static InventorySystem_Rasay.Inventory inventory = new InventorySystem_Rasay.Inventory();
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter PIN Code: ");
+        string userInput = Console.ReadLine();
+
+        while (userInput != "0")
+        {
+            if (userInput == pinCode)
+            {
+                Console.WriteLine("~ Welcome to Inventory ~");
+                Console.WriteLine("_____________________");
+                Console.WriteLine("Enter 1 to View Inventory");
+                Console.WriteLine("Enter 2 to Deposit Items");
+                Console.WriteLine("Enter 3 to Exit Inventory");
+                Console.WriteLine("-------------------------");
+                Console.Write("Enter your choice: ");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        inventory.ViewInventory();
+                        break;
+                    case "2":
+                        DepositItems();
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting Inventory...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid PIN Code. Please try again.");
+            }
+
+            Console.WriteLine("_____________________");
+            Console.Write("Do you want to continue (y/n)? ");
+            string continueChoice = Console.ReadLine();
+
+            if (continueChoice.ToLower() == "n")
+            {
+                Console.WriteLine("Exiting Inventory...");
+                return;
+            }
+
+            Console.Write("Enter PIN Code: ");
+            userInput = Console.ReadLine();
+        }
+    }
+
+    public static void DepositItems()
+    {
+        Console.WriteLine("~~~Deposit Items~~~");
+        Console.WriteLine("Enter the category [Snacks, Bath Essentials, Canned Goods, or Beverages]: ");
+        string category = Console.ReadLine();
+
+        Console.WriteLine("Enter the item name: ");
+        string itemName = Console.ReadLine();
+
+        inventory.DepositItems(category, itemName);
     }
 }
